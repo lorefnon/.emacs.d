3bb30953e970784ed6b6f1105e4e188a132c60d2
Lorefnon <lorefnon@gmail.com>
2018-01-07 02:03:06 +0530

    Cleanup

--- init.el
+++ init.el
@@ -14,17 +14,4 @@
 (require 'lf-lang-ts)
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(package-selected-packages
-   (quote
-    (helm-itunes winum avy move-lines undo-tree use-package tide spacemacs-theme neotree magit helm-projectile))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
