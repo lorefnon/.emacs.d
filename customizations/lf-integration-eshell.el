a32dda65b63d91b9cb5efefb68759a8c6c0aeb4e
Lorefnon <lorefnon@gmail.com>
2018-01-07 02:12:04 +0530

    Add eshell integration

---
+++ customizations/lf-integration-eshell.el
@@ -0,0 +1,7 @@
+(require 'lf-package-setup)
+
+(use-package multi-eshell
+  :bind (("C-c $ n" . multi-eshell)
+	 ("C-c $ s" . multi-eshell-switch)))
+
+(provide 'lf-integration-eshell)
--- init.el
+++ init.el
@@ -9,9 +9,23 @@
 (require 'lf-integration-magit)
 (require 'lf-integration-neotree)
 (require 'lf-integration-projectile)
+(require 'lf-integration-eshell)
 (require 'lf-utils-editing)
 (require 'lf-utils-nav)
 (require 'lf-lang-ts)
 
 (custom-set-variables
- '(initial-frame-alist (quote ((fullscreen . maximized)))))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (multi-eshell winum use-package undo-tree tide spacemacs-theme neotree move-text magit helm-projectile helm-itunes god-mode avy))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
