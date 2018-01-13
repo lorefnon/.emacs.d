45a3294b2b99ec9a423becb57e23a3a8f2b7cb3f
Lorefnon <lorefnon@gmail.com>
2018-01-13 12:58:24 +0530

    Configure projectile switch action to invoke dired along with projectile

--- config/lf-integration-neotree.el
+++ config/lf-integration-neotree.el
@@ -2,6 +2,7 @@
 
 (use-package neotree
   :bind (("C-c n t" . neotree-toggle)
-	 ("C-c n f" . neotree)))
+	 ("C-c n f" . neotree)
+	 ("C-c n ." . neotree-projectile-action)))
 
 (provide 'lf-integration-neotree)
--- config/lf-integration-projectile.el
+++ config/lf-integration-projectile.el
@@ -4,22 +4,36 @@
 
 (use-package projectile
   :defer t
-  :config (progn
-	    (projectile-global-mode t)))
+  :config
+  (progn
+    (projectile-global-mode t)))
 
 (use-package helm-projectile
+
   :init
   (require 'neotree)
-  (setq projectile-switch-project-action 'neotree-projectile-action)
-  :bind (("C-c ." . helm-projectile)
-	 ("C-." . helm-projectile)
-	 ("C-c p p" . projectile-switch-project)
-	 ("C-c p f" . helm-projectile-find-file-dwim)
-	 ("C-c p F" . helm-projectile-find-file-in-known-projects)
-	 ("C-c p r" . helm-projectile-recentf)
-	 ("C-c p a" . helm-projectile-ag)
-	 ("C-c p g" . helm-projectile-grep)
-	 ("C-c p b" . helm-projectile-switch-to-buffer))
+
+  (defun lf-projectile-action ()
+      "Open dired and neotree when switching projects"
+      (interactive)
+      (let ((root (projectile-project-root)))
+	(neotree-dir root)
+	(dired root)))
+
+  (setq projectile-switch-project-action 'lf-projectile-action)
+
+  :bind
+  (("C-c ." . helm-projectile)
+   ("C-." . helm-projectile)
+   ("C-c p ." . lf-projectile-action)
+   ("C-c p p" . projectile-switch-project)
+   ("C-c p f" . helm-projectile-find-file-dwim)
+   ("C-c p F" . helm-projectile-find-file-in-known-projects)
+   ("C-c p r" . helm-projectile-recentf)
+   ("C-c p a" . helm-projectile-ag)
+   ("C-c p g" . helm-projectile-grep)
+   ("C-c p b" . helm-projectile-switch-to-buffer))
+
   :config
   (require 'helm)
   (require 'projectile)
