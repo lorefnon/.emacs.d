596131dc6b9c57391c8fc1d9f70c452d8ba548b6
Lorefnon <lorefnon@gmail.com>
2018-01-14 16:35:50 +0530

    Add scala config

---
+++ config/lf-lang-scala.el
@@ -0,0 +1,13 @@
+(require 'lf-package-setup)
+
+(use-package ensime
+  :ensure t
+  :pin melpa)
+
+(use-package sbt-mode
+  :pin melpa)
+
+(use-package scala-mode
+  :pin melpa)
+
+(provide 'lf-lang-scala)
