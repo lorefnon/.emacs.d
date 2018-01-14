91a81b30b897c0388c8fa1c73536016595aedfc0
Lorefnon <lorefnon@gmail.com>
2018-01-14 16:38:54 +0530

    Better integration between neotree and switch-window

--- config/lf-integration-neotree.el
+++ config/lf-integration-neotree.el
@@ -1,8 +1,28 @@
 (require 'lf-package-setup)
 
 (use-package neotree
-  :bind (("C-c n t" . neotree-toggle)
-	 ("C-c n f" . neotree)
-	 ("C-c n ." . neotree-projectile-action)))
+  :bind
+
+  (("C-c n t" . neotree-toggle)
+   ("C-c n f" . neotree-find)
+   ("C-c n ." . neotree-projectile-action)
+   (:map neotree-mode-map
+	 ("M-<RET>" . lf-neotree-enter-in-switched-window)
+	 ("C-c M-w" . lf-neotree-copy-relative-path)))
+
+  :config
+
+  (defun lf-neotree-enter-in-switched-window ()
+    (interactive)
+    (let ((file (neo-buffer--get-filename-current-line)))
+      (switch-window)
+      (find-file file)))
+
+  (defun lf-neotree-copy-relative-path ()
+    (interactive)
+    (require 'f)
+    (let ((tree-file (neo-buffer--get-filename-current-line)))
+      (switch-window)
+      (kill-new (f-relative tree-file (f-dirname buffer-file-name))))))
 
 (provide 'lf-integration-neotree)
