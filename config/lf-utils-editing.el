3857858f869344c75661fd43af2bb23ca5e766b6
Lorefnon <lorefnon@gmail.com>
2018-01-14 16:37:55 +0530

    Configure indentation to use spaces

--- config/lf-utils-editing.el
+++ config/lf-utils-editing.el
@@ -1,5 +1,24 @@
 (require 'lf-package-setup)
 
+;; Enforce UTF-8 as the default coding system for all files, comint processes and buffers.
+;;
+;; Refer: https://www.masteringemacs.org/article/working-coding-systems-unicode-emacs
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+;; backwards compatibility as default-buffer-file-coding-system
+;; is deprecated in 23.2.
+(if (boundp 'buffer-file-coding-system)
+    (setq-default buffer-file-coding-system 'utf-8)
+  (setq default-buffer-file-coding-system 'utf-8))
+
+;; Tabs -> Spaces
+(setq-default indent-tabs-mode nil)
+
+;; Treat clipboard input as UTF-8 string first; compound text next, etc.
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
+
 (use-package undo-tree
   :config
   (global-undo-tree-mode))
@@ -8,9 +27,18 @@
   :config
   (move-text-default-bindings))
 
+(use-package s
+  :defer t)
+
+(use-package f
+  :defer t)
+
+(use-package dash
+  :defer t)
+
 (use-package disable-mouse
-  :bind (("C-c m m" . global-disable-mouse-mode))
-  )
+  :bind
+  (("C-c m m" . global-disable-mouse-mode)))
 
 (add-hook 'before-save-hook 'whitespace-cleanup)
 
@@ -21,22 +49,22 @@
 (defun lf-move-file (new-location)
   "Write this file to NEW-LOCATION, and delete the old one."
   (interactive (list (expand-file-name
-		      (if buffer-file-name
-			  (read-file-name "Move file to: ")
-			(read-file-name "Move file to: "
-					default-directory
-					(expand-file-name (file-name-nondirectory (buffer-name))
-							  default-directory))))))
+                      (if buffer-file-name
+                          (read-file-name "Move file to: ")
+                        (read-file-name "Move file to: "
+                                        default-directory
+                                        (expand-file-name (file-name-nondirectory (buffer-name))
+                                                          default-directory))))))
   (when (file-exists-p new-location)
     (delete-file new-location))
   (let ((old-location (expand-file-name (buffer-file-name))))
     (message "old file is %s and new file is %s"
-	     old-location
-	     new-location)
+             old-location
+             new-location)
     (write-file new-location t)
     (when (and old-location
-	       (file-exists-p new-location)
-	       (not (string-equal old-location new-location)))
+               (file-exists-p new-location)
+               (not (string-equal old-location new-location)))
       (delete-file old-location))))
 
 (defun xah-copy-file-path (&optional φdir-path-only-p)
@@ -47,20 +75,20 @@ URL `http://ergoemacs.org/emacs/emacs_copy_file_path.html'
 Version 2015-12-02"
   (interactive "P")
   (let ((ξfpath
-	 (if (equal major-mode 'dired-mode)
-	     (expand-file-name default-directory)
-	   (if (null (buffer-file-name))
-	       (user-error "Current buffer is not associated with a file.")
-	     (buffer-file-name)))))
+         (if (equal major-mode 'dired-mode)
+             (expand-file-name default-directory)
+           (if (null (buffer-file-name))
+               (user-error "Current buffer is not associated with a file.")
+             (buffer-file-name)))))
     (kill-new
      (if (null φdir-path-only-p)
-	 (progn
-	   (message "File path copied: 「%s」" ξfpath)
-	   ξfpath
-	   )
+         (progn
+           (message "File path copied: 「%s」" ξfpath)
+           ξfpath
+           )
        (progn
-	 (message "Directory path copied: 「%s」" (file-name-directory ξfpath))
-	 (file-name-directory ξfpath))))))
+         (message "Directory path copied: 「%s」" (file-name-directory ξfpath))
+         (file-name-directory ξfpath))))))
 
 
 (defun xah-delete-current-file ()
@@ -75,9 +103,9 @@ Version 2015-08-12"
     (kill-new (buffer-string))
     (when (buffer-file-name)
       (when (file-exists-p (buffer-file-name))
-	(progn
-	  (delete-file (buffer-file-name))
-	  (message "Deleted: 「%s」." (buffer-file-name)))))
+        (progn
+          (delete-file (buffer-file-name))
+          (message "Deleted: 「%s」." (buffer-file-name)))))
     (let ((buffer-offer-save nil))
       (set-buffer-modified-p nil)
       (kill-buffer (current-buffer)))))
@@ -92,26 +120,26 @@ Version 2015-08-12"
   "Keeps relative spacing in the region.  Unindents to the next multiple of the current tab-width"
   (interactive)
   (let ((deactivate-mark nil)
-	(beg (or (and mark-active (region-beginning)) (line-beginning-position)))
-	(end (or (and mark-active (region-end)) (line-end-position)))
-	(min-indentation)
-	(count (or count-arg 1)))
+        (beg (or (and mark-active (region-beginning)) (line-beginning-position)))
+        (end (or (and mark-active (region-end)) (line-end-position)))
+        (min-indentation)
+        (count (or count-arg 1)))
     (save-excursion
       (goto-char beg)
       (while (< (point) end)
-	(add-to-list 'min-indentation (current-indentation))
-	(forward-line)))
+        (add-to-list 'min-indentation (current-indentation))
+        (forward-line)))
     (if (< 0 count)
-	(if (not (< 0 (apply 'min min-indentation)))
-	    (error "Can't indent any more.  Try `indent-rigidly` with a negative arg.")))
+        (if (not (< 0 (apply 'min min-indentation)))
+            (error "Can't indent any more.  Try `indent-rigidly` with a negative arg.")))
     (if (> 0 count)
-	(indent-rigidly beg end (* (- 0 tab-width) count))
+        (indent-rigidly beg end (* (- 0 tab-width) count))
       (let (
-	    (indent-amount
-	     (apply 'min (mapcar (lambda (x) (- 0 (mod x tab-width))) min-indentation))))
-	(indent-rigidly beg end (or
-				 (and (< indent-amount 0) indent-amount)
-				 (* (or count 1) (- 0 tab-width))))))))
+            (indent-amount
+             (apply 'min (mapcar (lambda (x) (- 0 (mod x tab-width))) min-indentation))))
+        (indent-rigidly beg end (or
+                                 (and (< indent-amount 0) indent-amount)
+                                 (* (or count 1) (- 0 tab-width))))))))
 
 (global-set-key (kbd "C-c b n") 'generate-buffer)
 (global-set-key (kbd "C-c [") 'lf-unindent-dwim)
