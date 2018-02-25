ea0154a762af1ad4600adbda8a75da380134ad42
Lorefnon <lorefnon@gmail.com>
2018-02-25 19:56:32 +0530

    Improve navigation utilities and projectile integration

--- config/lf-integration-projectile.el
+++ config/lf-integration-projectile.el
@@ -20,6 +20,46 @@
         (neotree-dir root)
         (dired root)))
 
+  (defun lf-helm-projectile-do-with-file (action)
+    "Perform action for file found through projectile"
+    (let* ((project-root (projectile-project-root))
+           (project-files (projectile-select-files (projectile-current-project-files))))
+      (helm :sources (helm-build-sync-source "Projectile files"
+                       :candidates (helm-projectile--files-display-real project-files project-root)
+                       :fuzzy-match helm-projectile-fuzzy-match
+                       :action action)
+            :buffer "*helm projectile*"
+            :truncate-lines helm-projectile-truncate-lines
+            :prompt (projectile-prepend-project-name  "Find file :"))))
+
+  (defun lf-helm-projectile-kill-file-path ()
+    "Kill path of file found through helm in current project"
+    (interactive)
+    (lf-helm-projectile-do-with-file 'kill-new))
+
+  (defun lf-kill-rel-path (path)
+    "kill path relative to current buffer's file"
+    (kill-new (f-relative path (buffer-file-name))))
+
+  (defun lf-helm-projectile-kill-rel-file-path ()
+    "Kill relative path of file found through helm in current project"
+    (interactive)
+    (lf-helm-projectile-do-with-file 'lf-kill-rel-path))
+
+  (defun lf-f-relative (path1 path2)
+    (let* ((rel-path (f-relative path1 path2))
+           (is-rel (s-matches? "^\\.\\./" rel-path)))
+      (if is-rel rel-path (s-concat "./" rel-path))))
+
+  (defun lf-insert-rel-path (path)
+      "Insert base path of file relative to current buffer's file"
+      (insert (f-no-ext (lf-f-relative path (f-dirname (buffer-file-name))))))
+
+  (defun lf-helm-projectile-insert-rel-path ()
+    "Insert path of file in project found through helm relative to current buffer's file"
+    (interactive)
+    (lf-helm-projectile-do-with-file 'lf-insert-rel-path))
+
   (setq projectile-switch-project-action 'lf-projectile-action)
 
   :bind
@@ -31,7 +71,11 @@
    ("C-c p F" . helm-projectile-find-file-in-known-projects)
    ("C-c p r" . helm-projectile-recentf)
    ("C-c p g" . helm-projectile-grep)
-   ("C-c p b" . helm-projectile-switch-to-buffer))
+   ("C-c p b" . helm-projectile-switch-to-buffer)
+   ("C-c p w" . lf-helm-projectile-kill-rel-file-path)
+   ("C-c p W" . lf-helm-projectile-kill-file-path)
+   ("C-c p ~ ." . lf-helm-projectile-insert-rel-path)
+   ("C-c p ~ /" . lf-helm-projectile-insert-path))
 
   :config
   (require 'helm)
--- config/lf-utils-nav.el
+++ config/lf-utils-nav.el
@@ -32,4 +32,56 @@ position between `back-to-indentation' and `beginning-of-line'."
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
 
+(defadvice find-file (before make-directory-maybe (filename &optional wildcards) activate)
+  "Create parent directory if not exists while visiting file."
+  (unless (file-exists-p filename)
+    (let ((dir (file-name-directory filename)))
+      (unless (file-exists-p dir)
+        (make-directory dir)))))
+
+(defun lf-find-test-path-for-src-path (src-path)
+  "Get test path for source path"
+  (let* ((p1 (s-replace "/src" "/test" src-path))
+         (ext (f-ext src-path))
+         (repl-suffix-regex (s-concat  "\\." ext "$"))
+         (repl-new-suffix (s-concat "_spec." ext ))
+         (p2 (s-replace-regexp repl-suffix-regex repl-new-suffix p1)))
+    p2))
+
+(defun lf-find-src-path-for-test-path (src-path)
+  "Get test path for source path"
+  (let* ((p1 (s-replace "/test" "/src" src-path))
+         (ext (f-ext src-path))
+         (repl-suffix-regex (s-concat  "_spec\\." ext "$"))
+         (repl-new-suffix (s-concat "." ext ))
+         (p2 (s-replace-regexp repl-suffix-regex repl-new-suffix p1)))
+    p2))
+
+(defun lf-test-file? (path)
+  "Detect if file is test file"
+  (or
+   (s-contains? "/test" path)
+   (s-contains? "_spec" path)))
+
+(defun lf-nav-src-to-test ()
+  "Jump to test file for current source file"
+  (interactive)
+  (find-file (lf-find-test-path-for-src-path (buffer-file-name))))
+
+(defun lf-nav-test-to-src ()
+  "Jump to source file for current test file"
+  (interactive)
+  (find-file (lf-find-src-path-for-test-path (buffer-file-name))))
+
+(defun lf-nav-test-src-dwim ()
+  "Jump between source and test file"
+  (interactive)
+  (if (lf-test-file? (buffer-file-name))
+      (lf-nav-test-to-src)
+    (lf-nav-src-to-test)))
+
+(global-set-key (kbd "C-c G T") 'lf-nav-src-to-test)
+(global-set-key (kbd "C-c G S") 'lf-nav-test-to-src)
+(global-set-key (kbd "C-c G .") 'lf-nav-test-src-dwim)
+
 (provide 'lf-utils-nav)
