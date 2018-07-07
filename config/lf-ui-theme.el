a9bd14ecc18ccc0b184545da02250d8e09534e2b
Lorefnon <lorefnon@gmail.com>
2018-07-07 14:51:30 +0530

    Switch to nord-theme

--- config/lf-ui-theme.el
+++ config/lf-ui-theme.el
@@ -1,10 +1,12 @@
 (require 'lf-package-setup)
 
+(use-package nord-theme)
+
 ;; # Setup Primary Theme
-(use-package spacemacs-theme
-  :load-path "themes"
-  :config
-  (load-theme 'spacemacs-dark-theme t)
-  :no-require t)
+;; (use-package spacemacs-theme
+;;   :load-path "themes"
+;;   :config
+;;   (load-theme 'spacemacs-dark-theme t)
+;;   :no-require t)
 
 (provide 'lf-ui-theme)
